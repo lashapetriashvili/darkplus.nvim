8bf6b64f9f6f611c400b420ce0193ec43e816972
lasha petriashvili <lasha.petro1@gmail.com>
2023-03-06 21:33:36 +0400

    WIP

--- lua/darkplus/palette.lua
+++ lua/darkplus/palette.lua
@@ -1,8 +1,9 @@
 local colors = {
   fg = "#D4D4D4",
-  --[[ bg = "#1E1E1E", ]]
-  bg = "NONE",
+  bg = "#1E1E1E",
+  --[[ bg = "NONE", ]]
   alt_fg = "#a4a4a4",
+  --[[ alt_bg = "#252525", ]]
   alt_bg = "NONE",
   line = "#3e3e3e",
   dark_gray = "#404040",
